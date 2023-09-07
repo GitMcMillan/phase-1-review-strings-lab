378b85fe64d7b43aaabff1047768ce48c82c1689
Alexander <mcmillancontact@protonmail.com>
2023-09-07 06:45:55 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = `This Guy`;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
